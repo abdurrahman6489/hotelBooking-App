87eecfdeaba268e192d0cd75d8dab0626e688ec0
abdurrahman6489 <abdurrahman489@gmail.com>
2023-06-30 22:58:01 +0530

    pushing the Layout code and redirecting to login page when going to home without logging in

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,31 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HotelDetails from "./containers/HotelDetails";
 import Login from "./containers/Login";
 import Userprovider from "./components/Userprovider";
+import Layout from "./containers/Layout";
 
 function App() {
   return (
     <div>
       <Userprovider>
-        <Navbar />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Homepage />} />
-            <Route path="/hotel-details/:slug" element={<HotelDetails />} />
+            <Route
+              path="/home"
+              element={
+                <Layout>
+                  <Homepage />
+                </Layout>
+              }
+            />
+            <Route
+              path="/hotel-details/:slug"
+              element={
+                <Layout>
+                  <HotelDetails />
+                </Layout>
+              }
+            />
           </Routes>
         </BrowserRouter>
       </Userprovider>
